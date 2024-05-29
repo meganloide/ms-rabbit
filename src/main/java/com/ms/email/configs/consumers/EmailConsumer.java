fbbd56e74f91cbdd90182b32acea72bea5086a31
Gustavo Pereira <saijhon403@gmail.com>
2024-05-29 12:15:48 -0300

    first commit

---
+++ src/main/java/com/ms/email/configs/consumers/EmailConsumer.java
@@ -0,0 +1,29 @@
+package com.ms.email.configs.consumers;
+
+
+import com.ms.email.configs.dtos.EmailRecordDto;
+import com.ms.email.configs.services.EmailService;
+import com.ms.email.models.EmailModel;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @RabbitListener(queues = "${broker.queue.email.name}" )
+    public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+        emailService.sendEmail(emailModel);
+    }
+
+}
